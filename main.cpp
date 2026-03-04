7bb24c54b19e6378fdbbbd58e7c5242dfb61dfa6
ZDRAVKO ANEV <146438118+ZDRAVKO107UKTC@users.noreply.github.com>
2026-03-05 01:37:59 +0200

    Add GameManager class with singleton pattern and pause functionality in main function

--- main.cpp
+++ main.cpp
@@ -1,16 +1,57 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+class GameManager {
+    private:
+        bool isPaused;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    GameManager() : isPaused(false) {
+        std::cout << "GameManager initialized.\n";
+    }
+
+public:
+    GameManager(const GameManager&) = delete;
+    GameManager& operator=(const GameManager&) = delete;
+
+    static GameManager& getInstance() {
+        static GameManager instance;
+        return instance;
+    }
+
+    void togglePause() {
+        isPaused = !isPaused;
+        if (isPaused) {
+            std::cout << "[Game]: Game is now Paused!\n";
+        } else {
+            std::cout << "[Game]: Game is now Running!\n";
+        }
+    }
+
+    bool getIsPaused() const {
+        return isPaused;
+    }
+};
+
+int main() {
+    GameManager& gm1 = GameManager::getInstance();
+    GameManager& gm2 = GameManager::getInstance();
+
+    std::cout << "\nCalling togglePause() from gm1...\n";
+    gm1.togglePause();
+
+    std::cout << "Checking state from gm2: ";
+    if (gm2.getIsPaused()) {
+        std::cout << "Game is Paused!\n";
+    } else {
+        std::cout << "Game is Running!\n";
+    }
+
+    std::cout << "\n";
+
+    if (&gm1 == &gm2) {
+        std::cout << "SUCCESS: Both variables point to the same GameManager instance.\n";
+    } else {
+        std::cout << "FAILURE: Variables point to different instances!\n";
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
